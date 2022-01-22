e313df4c9d849aa68cdd2af341b7b811b90e9169
perpetualCreations <tchen0584@gmail.com>
2022-01-22 16:46:15 -0500

    Create service under system, safely use sudo bin.

--- systemdhelper.py
+++ systemdhelper.py
@@ -1,8 +1,9 @@
 from subprocess import call
 from enquiries import confirm
+from os.path import isfile
 
 if confirm("Create SYSTEMD unit?"):
-    with open("/etc/systemd/user/dl-jwtserver.service", "w") as writer:
+    with open("/etc/systemd/system/dl-jwtserver.service", "w") as writer:
         writer.write("""[Unit]
 Description=JWT authentication service over HTTP.
 After=network.target
@@ -12,6 +13,9 @@ ExecStart={}
 
 [Install]
 WantedBy=multi-user.target""".format(input("Enter path to start script: ")))
-    call("sudo systemctl enable dl-jwtserver", shell=True)
+    prefix = ""
+    if isfile("/bin/sudo"):
+        prefix = "sudo "
+    call(prefix + "systemctl enable dl-jwtserver", shell=True)
     if confirm("Start service?"):
-        call("sudo systemctl start dl-jwtserver", shell=True)
+        call(prefix + "systemctl start dl-jwtserver", shell=True)
